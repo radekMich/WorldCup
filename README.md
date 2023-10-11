b7dcb8f0f501be6b56c59cd8dce4dbe5e3faba81
Radek <radoslaw.michalkiewicz@mvp.superdevs.com>
2023-10-11 17:38:39 +0200

    Result on web page

--- README.md
+++ README.md
@@ -6,6 +6,15 @@
 2. Run command "composer install" (optional)
 3. run command "./vendor/bin/phpunit tests"
 
+## Run App
+
+1. You must have docker and docker-compose installed on your computer (https://docs.docker.com/compose/install/)
+2. Open terminal in project
+3. Run command "composer install"
+4. Run command "docker-compose build" if it doesn't work, "sudo docker-compose build"
+5. Run command "docker-compose up" if it doesn't work, "sudo docker-compose up"
+6. Open a browser on http://127.0.0.1:8000/ or http://localhost:8000/
+
 
 ### Add a README.md file where you can make notes of any assumption or things you would like to mention to us about your solution.
 
--- src/public/index.php
+++ src/public/index.php
@@ -1,3 +1,48 @@
 <?php
 
 require '../../vendor/autoload.php';
+
+$factory = new \App\Business\WorldCupBusinessFactory();
+
+$worldCup = $factory->createWorldCup();
+$mex = $factory->createTeam('Mexico');
+$ger = $factory->createTeam('Germany');
+$game1 = $factory->createGame($mex, $ger);
+
+$spa = $factory->createTeam('Spain');
+$port = $factory->createTeam('Portugal');
+$game2 = $factory->createGame($spa, $port);
+
+$ita = $factory->createTeam('Italy');
+$fra = $factory->createTeam('France');
+$game3 = $factory->createGame($ita, $fra);
+$worldCup->addGame($game1);
+$worldCup->addGame($game2);
+$worldCup->addGame($game3);
+foreach ($worldCup->getTournament() as $key => $game) {
+    $worldCup->startGame($key);
+}
+
+for ($i = 0; $i < 90; $i++) {
+    if (count($worldCup->getTournament()) == 0) {
+        break;
+    }
+    $randKey = array_rand($worldCup->getTournament());
+    $random = rand(0, 100);
+
+    if ($random < 10) {
+        $worldCup->goalHomeTeam($randKey);
+    } elseif ($random < 20) {
+        $worldCup->goalAwayTeam($randKey);
+    } elseif ($random % 50 == 0) {
+        $worldCup->finishGame($randKey);
+    }
+}
+
+foreach ($worldCup->getTournament() as $key => $game) {
+    $worldCup->finishGame($key);
+}
+
+foreach ($worldCup->getFinalResult() as $game) {
+    echo "<div>" . $game . "</div>";
+}
